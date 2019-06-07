bd0f47447b1034cf1ed5efcb2ebca4b4ab7a1853
lrajula <sushanth.laxmi@gmail.com>
2019-06-08 00:47:51 +0530

    Update notifyBuild.groovy

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -1,13 +1,13 @@
 def call(String buildStatus, String stageName, String git_author, String branch) {
    
-   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Branch: ${branch}"
    
    
    if (buildStatus == 'SUCCESS') {
      color = 'GREEN'
      colorCode = '#008000'
      summary = "SUCCESS: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} \n git_author: ${git_author} \n Branch: ${branch} <${env.BUILD_URL}/console|Click here for console log>)"
-     body = "The Build is Successful. Check out the log at ${env.BUILD_URL}"
+      body = "The Build is Successful. Check out the log at ${env.BUILD_URL}"
    } else if (buildStatus == 'FAILED') {
      color = 'RED'
      colorCode = '#FF0000'
