891c645901533980d5ad987b995a7457791a4a15
lrajula <sushanth.laxmi@gmail.com>
2019-06-08 00:50:40 +0530

    Update notifyBuild.groovy

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -11,17 +11,17 @@ def call(String buildStatus, String stageName, String git_author, String branch)
    } else if (buildStatus == 'FAILED') {
      color = 'RED'
      colorCode = '#FF0000'
-     summary = "FAILED: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} in stage ${stageName} git_author ${git_author} Branch ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
+     summary = "FAILED: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} in stage ${stageName}  \n git_author: ${git_author} \n Branch: ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
      body = "The Build is Failed in stage ${stageName}. Check out the log at ${env.BUILD_URL}"
    } else if (buildStatus == 'UNSTABLE') {
      color = 'YELLOW'
      colorCode = '#FFFF00'
-     summary = "UNSTABLE: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} git_author ${git_author} Branch ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
+     summary = "UNSTABLE: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER}  \n git_author: ${git_author} \n Branch: ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
      body = "The Build is Unstable. Check out the log at ${env.BUILD_URL}"
    } else if (buildStatus == 'ABORTED') {
      color = 'GREY'
      colorCode = '#808080'
-     summary = "ABORTED: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} in stage ${stageName}  git_author ${git_author} Branch ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
+     summary = "ABORTED: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} in stage ${stageName} \n git_author: ${git_author} \n Branch: ${branch} (<${env.BUILD_URL}/console|Click here for console log>)"
      body = "The Build was Aborted at ${stageName}. Check out the log at ${env.BUILD_URL}"
    }
  
